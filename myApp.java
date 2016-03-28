4341f51551ea73289ed5af1361b49949907e455f
Veijo Väisänen <veijo.vaisanen@oamk.fi>
2016-03-28 12:15:47 +0300

    Added changePrice

--- myApp.java
+++ myApp.java
@@ -2,6 +2,7 @@
 class main {
     public static void main(String[] args) { 
     	Product product = new Product("Edam", 3.3, 120);
+		product.changePrice(3.1);
     	System.out.println("Product value is " + product.countValue());
     	product.printProduct();
      }
@@ -22,5 +23,9 @@ class main {
  
 	public void printProduct() {
 		System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
+	}
+	
+	public void changePrice(double value) {
+		price = value;
 	}
 }
