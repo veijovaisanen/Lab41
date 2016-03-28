e39d68824816f8d374d281bc734ebef8ab0ff1b4
Veijo Väisänen <veijo.vaisanen@oamk.fi>
2016-03-28 12:31:18 +0300

    Back to basic

--- myApp.java
+++ myApp.java
@@ -2,8 +2,6 @@
 class main {
     public static void main(String[] args) { 
     	Product product = new Product("Edam", 3.3, 120);
-		product.changeBalance(200);
-		product.changePrice(3.1);
     	System.out.println("Product value is " + product.countValue());
     	product.printProduct();
      }
@@ -25,12 +23,4 @@ class main {
 	public void printProduct() {
 		System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
 	}
-	
-
-	public void changeBalance(int value) {
-		amount = value;
-	}
-	public void changePrice(double value) {
-		price = value;
-	}
-}
+ }
